6c30f7a20d35ff67fc6a1c8c3887d3cfd5d7dd6b
James Keveren <james@keve.ren>
2019-12-30 23:02:48 +0100

    reduced quality for higher frmaerate

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 const c = document.documentElement
 	.appendChild(document.createElement('canvas'))
-	.getContext('2d')
+	.getContext('2d', {
+		alpha: false,
+		desynchronized: true,
+	})
 ;
 Object.assign(c.canvas.style, {
 	position: 'fixed',
@@ -9,21 +12,29 @@ Object.assign(c.canvas.style, {
 	height: '100vh',
 });
 
+let imageData;
+
 const resize = () => {
 	c.canvas.width = innerWidth * devicePixelRatio;
 	c.canvas.height = innerHeight * devicePixelRatio;
+	imageData = new ImageData(new Uint8ClampedArray(c.canvas.width * c.canvas.height * 4), c.canvas.width);
 };
 resize();
 addEventListener('resize', resize);
 
+
 const render = async () => {
 	const t = Date.now();
-	const imageData = new ImageData(new Uint8ClampedArray(c.canvas.width * c.canvas.height * 4), c.canvas.width);
+	let randomBits;
 	for (let i = 0; i < imageData.data.length; i += 4) {
-		const random = Math.random() * 255;
-		imageData.data[i] = random;
-		imageData.data[i + 1] = random;
-		imageData.data[i + 2] = random;
+		if (!(i % 16)) {
+			randomBits = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+		}
+		randomBits >>>= 2;
+		const randomBrightness = randomBits & 0b11 * 85;
+		imageData.data[i] = randomBrightness;
+		imageData.data[i + 1] = randomBrightness;
+		imageData.data[i + 2] = randomBrightness;
 		imageData.data[i + 3] = 255;
 	}
 	const imageBitmap = await createImageBitmap(imageData, 0, 0, imageData.width, imageData.height);
