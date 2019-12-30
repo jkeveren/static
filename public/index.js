cd2d8a438fd47da097d54e868a8c1dcbfee0e403
James Keveren <james@keve.ren>
2019-12-30 21:00:17 +0100

    inneficient 2d context method

--- public/index.js
+++ public/index.js
@@ -1,88 +1,32 @@
-const canvas = document.documentElement.appendChild(document.createElement('canvas'));
-Object.assign(canvas.style, {
+const c = document.documentElement.appendChild(document.createElement('canvas')).getContext('2d');
+Object.assign(c.canvas.style, {
 	position: 'fixed',
 	top: 0,
 	width: '100vw',
 	height: '100vh',
 });
 
-const gl = canvas.getContext('webgl2');
-gl.clearColor(0, 0, 0, 1);
-
 const resize = () => {
-	canvas.width = innerWidth * devicePixelRatio;
-	canvas.height = innerHeight * devicePixelRatio;
-	gl.viewport(0, 0, canvas.width, canvas.height);
+	c.canvas.width = innerWidth * devicePixelRatio;
+	c.canvas.height = innerHeight * devicePixelRatio;
 };
 resize();
 addEventListener('resize', resize);
 
-const shaders = [
-	[
-		gl.VERTEX_SHADER,
-		`#version 300 es
-
-			void main(void) {
-				gl_PointSize = 1.0;
-				gl_Position = vec4(0, 0, 0, 1.0);
-			}
-		`
-	],
-	[
-		gl.FRAGMENT_SHADER,
-		`#version 300 es
-			precision mediump float;
-			out vec4 finalColor;
-
-			void main(void) {
-				finalColor = vec4(1, 1, 1, 1);
-			}
-		`
-	]
-];
-
-for (const [i, [shaderType, shaderSource]] of Object.entries(shaders)) {
-	const shader = shaders[i] = gl.createShader(shaderType);
-	gl.shaderSource(shader, shaderSource);
-	gl.compileShader(shader);
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		throw new Error(gl.getShaderInfoLog(shader));
-	}
-}
-
-const program = gl.createProgram();
-for (const shader of shaders) {
-	gl.attachShader(program, shader);
-}
-gl.linkProgram(program);
-
-if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-	throw new Error(gl.getProgramInfoLog(program));
-}
-
-gl.validateProgram(program);
-if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-	throw new Error(gl.getPropreamInfoLog(program));
-}
-
-for (const shader of shaders) {
-	gl.detachShader(program, shader);
-	gl.deleteShader(shader);
-}
-
 const render = () => {
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-	const verticesFloatArray = new Float32Array([]);
-	const verticesBuffer = gl.createBuffer();
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesFloatArray, gl.STATIC_DRAW); ///////////// change gl.STATIC_DRAW
-	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-	gl.useProgram(program);
-
-	gl.drawArrays(gl.POINTS, 0, 1);
+	const t = Date.now();
+	for (let x = 0; x < c.canvas.width; x++) {
+		for (let y = 0; y < c.canvas.height; y++) {
+			c.fillStyle = Math.round(Math.random()) ? '#fff' : '#000';
+			c.fillRect(x, y, 1, 1);
+		}
+	}
+// 	c.drawImage(
+// 		,
+// 		0,
+// 		0
+// 	);
+	console.log(Date.now() - t);
 	requestAnimationFrame(render);
 }
 requestAnimationFrame(render);
