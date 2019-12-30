33c8a77e5ed820c0b7919841a1d287225165d040
James Keveren <james@keve.ren>
2019-12-30 23:25:23 +0100

    reduced resolution for authenticity and performance. added better frmaerate counter hidden cursor

--- public/index.js
+++ public/index.js
@@ -5,26 +5,33 @@ const c = document.documentElement
 		desynchronized: true,
 	})
 ;
+
 Object.assign(c.canvas.style, {
 	position: 'fixed',
 	top: 0,
 	width: '100vw',
 	height: '100vh',
+	cursor: 'none'
 });
 
 let imageData;
-
+const resolutionMultiplier = 0.5;
 const resize = () => {
-	c.canvas.width = innerWidth * devicePixelRatio;
-	c.canvas.height = innerHeight * devicePixelRatio;
+	c.canvas.width = innerWidth * devicePixelRatio * resolutionMultiplier;
+	c.canvas.height = innerHeight * devicePixelRatio * resolutionMultiplier;
 	imageData = new ImageData(new Uint8ClampedArray(c.canvas.width * c.canvas.height * 4), c.canvas.width);
 };
 resize();
 addEventListener('resize', resize);
 
+let frames = 0;
+setInterval(() => {
+	console.clear();
+	console.log(`Framerate: ${frames}`);
+	frames = 0;
+}, 1000);
 
 const render = async () => {
-	const t = Date.now();
 	let randomBits;
 	for (let i = 0; i < imageData.data.length; i += 4) {
 		if (!(i % 16)) {
@@ -39,7 +46,7 @@ const render = async () => {
 	}
 	const imageBitmap = await createImageBitmap(imageData, 0, 0, imageData.width, imageData.height);
 	c.drawImage(imageBitmap, 0, 0);
-	console.log(Date.now() - t);
+	frames++;
 	requestAnimationFrame(render);
 }
 requestAnimationFrame(render);
